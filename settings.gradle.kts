5e159f1fec370ac850e00bd55b88d07b0b61f6cb
Eric Haag <ehaag@gradle.com>
2023-02-08 16:57:21 -0600

    Connect to Gradle Enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,27 @@
-rootProject.name = "gradle-parallel-task-execution-reproducer"
+plugins {
+  id("com.gradle.enterprise") version "3.12.3"
+  id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.2"
+}
+
+val isCI = !System.getenv("CI").isNullOrEmpty()
+
+gradleEnterprise {
+  server = "https://ge.solutions-team.gradle.com"
+  buildScan {
+    capture { isTaskInputFiles = true }
+    isUploadInBackground = !isCI
+    publishAlways()
+  }
+}
+
+buildCache {
+  local {
+    isEnabled = true
+  }
+  remote(gradleEnterprise.buildCache) {
+    isEnabled = true
+    isPush = isCI
+  }
+}
+
+rootProject.name = "gradle-parallel-task-execution-sample"
